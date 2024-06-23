135e4216634493b79657a412dff429585db81f12
livinginatent <suleyman.eminbeyli@gmail.com>
2024-06-23 22:52:35 +0400

    implementing weekly forecast

--- src/components/MainDetails/WeeklyForecast/Day.tsx
+++ src/components/MainDetails/WeeklyForecast/Day.tsx
@@ -1,42 +1,16 @@
 import { DailyForecastT } from "@/lib/types";
 import React from "react";
+import { IoSunnyOutline } from "react-icons/io5";
 
 type Props = {};
 
-function Day(data: DailyForecastT,index:number) {
-  const weatherData = data.forecast.forecastday[index]
-  return (
-    <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-      <div className="p-4">
-        <h2 className="text-2xl font-semibold text-gray-800">
-          10-Day Weather Forecast
-        </h2>
-      </div>
-      <div>
-        <div className="flex items-center justify-between p-4 border-t border-gray-200">
-          <div className="flex items-center space-x-2">
-            <span className="font-medium text-gray-700">{'Friday'}</span>
-            <span className="text-gray-500">{weatherData.date}</span>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="flex items-center">
-              <span className="text-xl font-bold text-gray-800">
-                {weatherData.day.maxtemp_c}°
-              </span>
-              <span className="text-gray-500"> / {weatherData.day.mintemp_c}°</span>
-            </div>
-            <div className="flex flex-col">
-              {/* <span className="text-gray-700">{day.description}</span>
-              <span className="text-gray-500">{day.nightDescription}</span> */}
-            </div>
-            <div className="flex items-center space-x-1">
-              <span className="text-gray-500">{weatherData.day.totalprecip_mm}%</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+function Day(/* data: DailyForecastT, index: number */) {
+  const sunIcon = <IoSunnyOutline/>
+  return <div className="border-r-4 flex flex-col w-32 border-indigo-500 ...">
+    <p>Monday</p>
+    {sunIcon}
+
+  </div>;
 }
 
 export default Day;
--- src/components/MainDetails/WeeklyForecast/WeeklyForecast.tsx
+++ src/components/MainDetails/WeeklyForecast/WeeklyForecast.tsx
@@ -1,43 +1,52 @@
 import useWeatherStore from "@/store/store";
-import React from "react";
-
+import React, { useEffect } from "react";
+import {
+  Area,
+  CartesianGrid,
+  ComposedChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import Day from "./Day";
 
 const WeeklyForecast = () => {
-    const searchCity = useWeatherStore((state) => state.coordinates);
-    const { showHourlyForecast, setShowHourlyForecast } = useWeatherStore();
-
-    const handleClick = (showHourly: boolean) => {
-      setShowHourlyForecast(showHourly);
-    };
-
-    useEffect(() => {
-      const fetchWeatherData = async () => {
-        setLoading(true);
-        try {
-          let data;
-          if (searchCity.lat != null && searchCity.lon != null) {
-            data = await getSearchCityHourly({
-              lat: searchCity.lat,
-              lon: searchCity.lon,
-            });
-          } else {
-            data = await getHourly();
-          }
-
-          if (data) {
-            setHourlyWeatherData(data);
-          }
-        } catch (error) {
-          console.error("Error fetching weather data", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-
-      fetchWeatherData();
-    }, [searchCity]);
+  const data = [
+    { date: "23 Jun", temp: 22 },
+    { date: "24 Jun", temp: 25 },
+    { date: "25 Jun", temp: 20 },
+    { date: "26 Jun", temp: 27 },
+    { date: "27 Jun", temp: 24 },
+    { date: "28 Jun", temp: 23 },
+    { date: "29 Jun", temp: 26 },
+  ];
   return (
-   <div></div>
+    <div className="w-3/4 h-96 flex flex-col border border-[#F7F9F2] rounded-xl bg-red  bg-white">
+      <ResponsiveContainer width="100%" height={195}>
+        <ComposedChart
+          data={data}
+          margin={{ top: 20, right: 10, bottom: 10, left: 10 }}
+        >
+          <Tooltip active={false} />
+          <XAxis
+            padding={{ left: 25, right: 25 }}
+            interval={0}
+            dataKey={"date"}
+            tickLine={false}
+            axisLine={false}
+          />
+          <Area
+            type="monotone"
+            dataKey="temp"
+            fill="#eceeff"
+            stroke="#77bae8"
+            label={{ position: "top", fill: "#333" }}
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+      <Day/>
+    </div>
   );
 };
 
