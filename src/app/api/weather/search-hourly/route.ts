e67874e863f6477064962b8a51c3e5b1ec78447e
livinginatent <suleyman.eminbeyli@gmail.com>
2024-07-11 17:07:29 +0400

    CORS & favicon

--- src/app/api/weather/search-hourly/route.ts
+++ src/app/api/weather/search-hourly/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
   const API_KEY = process.env.API_KEY;
   const { searchParams } = new URL(request.url);
@@ -8,12 +10,34 @@ export async function GET(request: Request) {
   const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${lat},${lon}&aqi=yes`;
 
   try {
+    const headers = {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+      "Surrogate-Control": "no-store",
+      "Access-Control-Allow-Origin": "*", // Allow all origins
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow specific methods
+      "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow specific headers
+    };
     const res = await fetch(url, { cache: "no-store" });
     const data = await res.json();
-    return new Response(JSON.stringify(data), { status: 200 });
+    return new Response(JSON.stringify(data), { status: 200,headers });
   } catch (error) {
-    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
-      status: 500,
-    });
+   const headers = {
+     "Content-Type": "application/json",
+     "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+     Pragma: "no-cache",
+     Expires: "0",
+     "Surrogate-Control": "no-store",
+     "Access-Control-Allow-Origin": "*", // Allow all origins
+     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow specific methods
+     "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow specific headers
+   };
+
+   return new NextResponse(JSON.stringify({ error: "Failed to fetch data" }), {
+     status: 500,
+     headers,
+   });
   }
 }
