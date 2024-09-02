052f5fd5ef0aa26d7f52ea79ec45a697e40686e8
livinginatent <suleyman.eminbeyli@gmail.com>
2024-09-02 08:48:31 +0400

    sitemap fixed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://acme.com/calendar",
+      url: "https://havam.az/calendar",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
