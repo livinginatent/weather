64484115bcefc8613a46f01a1ab8f9351b94118c
livinginatent <suleyman.eminbeyli@gmail.com>
2024-11-26 16:43:08 +0400

    sidebar loader changed

--- src/components/SideDetails/SideDetailsMainInfo/SideDetailsMainInfo.tsx
+++ src/components/SideDetails/SideDetailsMainInfo/SideDetailsMainInfo.tsx
@@ -4,7 +4,8 @@ import { CiLocationOn, CiCalendar } from "react-icons/ci";
 import { MainDetailsT } from "./types";
 import Image from "next/image";
 import "react-loading-skeleton/dist/skeleton.css";
-import {  SyncLoader } from "react-spinners";
+import { MoonLoader, SyncLoader } from "react-spinners";
+
 const SideDetailsMainInfo = ({
   city,
   temp,
@@ -12,44 +13,45 @@ const SideDetailsMainInfo = ({
   date,
   logo,
 }: MainDetailsT) => {
- 
+  // Check if any prop is missing
+  const isLoading = !city || !temp || !condition || !date;
+
   return (
     <div className="flex flex-col w-full gap-2 justify-center items-center mt-10">
-      <>
-        <div className="flex justify-center items-center gap-1 flex-col ">
+      {isLoading ? (
+        <MoonLoader color="white" />
+      ) : (
+        <>
+          <div className="flex justify-center items-center gap-1 flex-col ">
             <h2 className="text-slate-50 text-2xl sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
               Hava Proqnozu
             </h2>
-          <CiLocationOn color="white" size={32} />
-          <div>
-            <p className="text-slate-50 text-2xl sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
-              {city?.length ? city : <SyncLoader color="white" />}
-            </p>
+            <CiLocationOn color="white" size={32} />
+            <div>
+              <p className="text-slate-50 text-2xl sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
+                {city}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center items-center mt-10">
-          <div className="text-8xl text-slate-50">
-            {temp?.length ? temp : <SyncLoader color="white" />}
+          <div className="flex justify-center items-center mt-10">
+            <div className="text-8xl text-slate-50">{temp}</div>
           </div>
-        </div>
-        <div className="flex flex-col justify-center items-center lg:gap-2 xl:gap-2 gap-4">
-          <div className="flex justify-center items-center self-center lg:gap-1 xl:gap-1 sm:gap-4 md:gap-4">
-            {condition?.length ? (
+          <div className="flex flex-col justify-center items-center lg:gap-2 xl:gap-2 gap-4">
+            <div className="flex justify-center items-center self-center lg:gap-1 xl:gap-1 sm:gap-4 md:gap-4">
               <Image alt={`${condition}`} src={logo} width={64} height={64} />
-            ) : null}
-
-            <p className="text-slate-50 text-lg md:text-lg lg:text-xl xl:text-xl font-light">
-              {condition}
-            </p>
+              <p className="text-slate-50 text-lg md:text-lg lg:text-xl xl:text-xl font-light">
+                {condition}
+              </p>
+            </div>
+            <div className="flex self-center gap-2 lg:pt-5 xl:pt-5">
+              <CiCalendar color="white" size={24} />
+              <p className="text-slate-50 s text-lg md:text-lg lg:text-xl xl:text-xl">
+                {date}
+              </p>
+            </div>
           </div>
-          <div className="flex self-center gap-2 lg:pt-5 xl:pt-5">
-            <CiCalendar color="white" size={24} />
-            <p className="text-slate-50 s text-lg md:text-lg lg:text-xl xl:text-xl">
-              {date?.length ? date : <SyncLoader color="white" />}
-            </p>
-          </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
