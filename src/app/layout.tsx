4ebb10b4a5a246e724e5536aa8b6b2508418f4c7
livinginatent <suleyman.eminbeyli@gmail.com>
2024-12-05 18:42:48 +0400

    schema test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,43 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    url: "https://havam.az",
+    name: "Havam.az",
+    description: "Havam.az - Hava proqnozu və keyfiyyəti haqqında məlumatlar.",
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://havam.az/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": "https://havam.az",
+    },
+    sitelinksSearchBox: {
+      "@type": "SearchAction",
+      target: "https://havam.az/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+    about: {
+      "@type": "Thing",
+      name: "Hava Proqnozu və Keyfiyyəti",
+    },
+    relatedLink: [
+      {
+        "@type": "WebPage",
+        name: "Hava Keyfiyyəti",
+        url: "https://havam.az/aqi",
+      },
+      {
+        "@type": "WebPage",
+        name: "Aylıq Hava Proqnozu",
+        url: "https://havam.az/monthly",
+      },
+    ],
+  };
   return (
     <html className="h-full" lang="en" suppressHydrationWarning>
       <head>
@@ -23,6 +60,7 @@ export default function RootLayout({
           name="google-site-verification"
           content="AiTKc0Y6143pH0fcu062C9n_f3gvdRVGLSrW6g3xE2k"
         />
+        <script type="application/ld+json">{JSON.stringify(schema)}</script>
       </head>
 
       <body
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,48 +10,10 @@ export const metadata: Metadata = {
     "Bakı və digər şəhərlərin hava proqnozu Havam.az ilə qarşınızda. Günlük, həftəlik hava proqnozu. Bakıda hava.",
 };
 export default function Home() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "WebSite",
-    url: "https://havam.az",
-    name: "Havam.az",
-    description: "Havam.az - Hava proqnozu və keyfiyyəti haqqında məlumatlar.",
-    potentialAction: {
-      "@type": "SearchAction",
-      target: "https://havam.az/search?q={search_term_string}",
-      "query-input": "required name=search_term_string",
-    },
-    mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": "https://havam.az",
-    },
-    sitelinksSearchBox: {
-      "@type": "SearchAction",
-      target: "https://havam.az/search?q={search_term_string}",
-      "query-input": "required name=search_term_string",
-    },
-    about: {
-      "@type": "Thing",
-      name: "Hava Proqnozu və Keyfiyyəti",
-    },
-    relatedLink: [
-      {
-        "@type": "WebPage",
-        name: "Hava Keyfiyyəti",
-        url: "https://havam.az/aqi",
-      },
-      {
-        "@type": "WebPage",
-        name: "Aylıq Hava Proqnozu",
-        url: "https://havam.az/monthly",
-      },
-    ],
-  };
+  
   return (
     <>
-      <Head>
-        <script type="application/ld+json">{JSON.stringify(schema)}</script>
-      </Head>
+     
       <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row custom-height-903">
         <SideDetails />
         <MainDetails />
