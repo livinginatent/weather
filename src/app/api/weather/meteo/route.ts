01eacfa29fe6fa9580b873e59bbfa274702a40a1
livinginatent <suleyman.eminbeyli@gmail.com>
2024-12-20 12:24:41 +0400

    meteo api no cache

--- src/app/api/weather/meteo/route.ts
+++ src/app/api/weather/meteo/route.ts
@@ -1,34 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fetchWeatherApi } from "openmeteo";
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const lat = searchParams.get("lat");
   const lon = searchParams.get("lon");
   const date = searchParams.get("date");
+
   const params = {
     latitude: lat,
     longitude: lon,
     start_date: date,
     end_date: date,
-
-    daily: ["weather_code", "apparent_temperature_max", "apparent_temperature_min", "rain_sum", "snowfall_sum", "wind_speed_10m_max"],
+    daily: [
+      "weather_code",
+      "apparent_temperature_max",
+      "apparent_temperature_min",
+      "rain_sum",
+      "snowfall_sum",
+      "wind_speed_10m_max",
+    ],
     timezone: "Europe/Moscow",
   };
 
   const url = "https://archive-api.open-meteo.com/v1/archive";
   const responses = await fetchWeatherApi(url, params);
+
   const range = (start: number, stop: number, step: number) =>
     Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
   try {
     const response = responses[0];
-
-    // Attributes for timezone and location
     const utcOffsetSeconds = response.utcOffsetSeconds();
- 
-
     const daily = response.daily()!;
 
-    // Note: The order of weather variables in the URL query and the indices below need to match!
     const weatherData = {
       daily: {
         time: range(
@@ -45,7 +50,12 @@ export async function GET(request: NextRequest) {
       },
     };
 
-    return NextResponse.json(weatherData);
+    return NextResponse.json(weatherData, {
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+      },
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
