6bc10318747d7fbfcd1a792d902ee5cb62f3472d
livinginatent <suleyman.eminbeyli@gmail.com>
2025-01-22 22:33:50 +0400

    post body styling

--- src/components/SanityBlocks/SanityBlock.tsx
+++ src/components/SanityBlocks/SanityBlock.tsx
@@ -6,30 +6,51 @@ import React from "react";
 
 export const SanityBlocks = {
   types: {
-    image: ({ value }:any) => (
-      <Image
-        src={urlFor(value).url()}
-        alt={value.alt || "Image"}
-        style={{ maxWidth: "100%", height: "auto" }}
-      />
+    image: ({ value }: any) => (
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "1em 0" }}
+      >
+        <Image
+          src={urlFor(value).url()}
+          alt={value.alt || "Image"}
+          width={1000}
+          height={100}
+          style={{ objectFit: "contain" }}
+        />
+      </div>
     ),
   },
   block: {
     // Customize block types (e.g., paragraphs, headings)
-    h1: ({ children }:any) => (
-      <h1 style={{ fontSize: "2em", color: "#333" }}>{children}</h1>
+    h1: ({ children }: any) => (
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "1em 0" }}
+      >
+        <h1 style={{ fontSize: "2em", color: "#333" }}>{children}</h1>
+      </div>
     ),
-    h2: ({ children }:any) => (
-      <h2 style={{ fontSize: "1.9em", color: "black" }}>{children}</h2>
+    h2: ({ children }: any) => (
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "1em 0" }}
+      >
+        <h2 style={{ fontSize: "1.9em", color: "black" }}>{children}</h2>
+      </div>
     ),
-    h3: ({ children }:any) => (
-      <h3 style={{ fontSize: "1.6em", color: "black" }}>{children}</h3>
+    h3: ({ children }: any) => (
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "1em 0" }}
+      >
+        <h3 style={{ fontSize: "1.6em", color: "black" }}>{children}</h3>
+      </div>
     ),
-    h4: ({ children }:any) => (
+    h4: ({ children }: any) => (
+      
       <h4 style={{ fontSize: "1em", color: "black" }}>{children}</h4>
     ),
-    normal: ({ children }:any) => <p style={{ lineHeight: "1.6" }}>{children}</p>,
-    blockquote: ({ children }:any) => (
+    normal: ({ children }: any) => (
+      <p style={{ lineHeight: "1.6" }}>{children}</p>
+    ),
+    blockquote: ({ children }: any) => (
       <blockquote style={{ fontStyle: "italic", margin: "1em 0" }}>
         {children}
       </blockquote>
@@ -37,11 +58,13 @@ export const SanityBlocks = {
   },
   marks: {
     // Customize text styles (e.g., bold, italic)
-    strong: ({ children }:any) => (
+    strong: ({ children }: any) => (
       <strong style={{ fontWeight: "bold" }}>{children}</strong>
     ),
-    em: ({ children }:any) => <em style={{ fontStyle: "italic" }}>{children}</em>,
-    link: ({ value, children }:any) => {
+    em: ({ children }: any) => (
+      <em style={{ fontStyle: "italic" }}>{children}</em>
+    ),
+    link: ({ value, children }: any) => {
       const target = value?.href?.startsWith("http") ? "_blank" : undefined;
       return (
         <a
@@ -56,17 +79,17 @@ export const SanityBlocks = {
   },
   list: {
     // Customize list styles
-    bullet: ({ children }:any) => (
+    bullet: ({ children }: any) => (
       <ul style={{ listStyleType: "disc", marginLeft: "1em" }}>{children}</ul>
     ),
-    number: ({ children }:any) => (
+    number: ({ children }: any) => (
       <ol style={{ listStyleType: "decimal", marginLeft: "1em" }}>
         {children}
       </ol>
     ),
   },
   listItem: {
-    bullet: ({ children }:any) => <li>{children}</li>,
-    number: ({ children }:any) => <li>{children}</li>,
+    bullet: ({ children }: any) => <li>{children}</li>,
+    number: ({ children }: any) => <li>{children}</li>,
   },
 };
