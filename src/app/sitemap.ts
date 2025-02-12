d8d4950f5b23f8fe8811121f2a37e19bcdedde8f
livinginatent <suleyman.eminbeyli@gmail.com>
2025-02-12 14:34:54 +0400

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -65,6 +65,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: "https://havam.az/quba",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 
   // Dynamic routes for posts
